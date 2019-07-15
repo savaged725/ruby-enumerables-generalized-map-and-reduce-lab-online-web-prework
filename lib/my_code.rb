eaf4d73f0c232ab100993dcf93484ee9d148a84d
Deborah Savage <savaged725@yahoo.com>
2019-07-15 22:18:38 +0000

    Done.

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,10 @@
-# Your Code Here
+def make_sandwich(element1, element2)
+  base = "A #{element1} and #{element2}"
+  yield(base)
+end
+ 
+make_sandwich("gator", "gumbo") do |innards|
+  "#{innards} on rye"
+end #
+
+map_to_negativize ({1, 2, 3, -9})
