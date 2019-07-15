99003095a49c17b75c14065fd1cd87ef94945730
Deborah Savage <savaged725@yahoo.com>
2019-07-15 22:15:58 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,8 @@
-# Your Code Here
+def make_sandwich(element1, element2)
+  base = "A #{element1} and #{element2}"
+  yield(base)
+end
+ 
+make_sandwich("gator", "gumbo") do |innards|
+  "#{innards} on rye"
+end #
